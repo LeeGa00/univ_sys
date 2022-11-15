18753dacd0a966ccf82f148885b27c83e0396ec0
gayoung <artist158@naver.com>
2022-11-15 11:23:35 +0900

    2002.11.23 git clear

--- sys_hack/stack7.c
+++ sys_hack/stack7.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void *getpath(void) {
+char *getpath(void) {
 	char buffer[64];
 	unsigned int ret;
 
@@ -15,17 +15,17 @@ void *getpath(void) {
 	ret = __builtin_return_address(0);
 
 	if((ret & 0xbf000000) == 0xbf000000) {
-		printf("bzzzt (%p)", ret);
-		_exit(1);
-	}
+                printf("bzzzt (%p)", ret);
+                _exit(1);
+        }
 
-	printf("got path %s\n", buffer);
+        printf("got path %s", buffer);
 
 	return strdup(buffer);
 }
 
 int main(int argc, char **argv){
-	getpath();
+        getpath();
 
-	return(0);
+        return(0);
 }
